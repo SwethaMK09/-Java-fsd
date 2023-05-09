6ac07999ba4a2078a6a51edf9bfb9f8991cf7e44
Swetha M K <106295169+SwethaMK09@users.noreply.github.com>
2023-05-09 07:43:08 +0530

    Delete Practice Projects Assisted directory

--- Practice Projects Assisted/Practice Project Assisted_2/DefaultAccess.java
+++
@@ -1,8 +0,0 @@
-
-public class DefaultAccess {
-	void display() 
-    { 
-        System.out.println("You are using default access specifier"); 
-    } 
-}
-
--- Practice Projects Assisted/Practice Project Assisted_2/package2/procaccess.java
+++
@@ -1,8 +0,0 @@
-package package2;
-import propackage.*;
-public class procaccess extends ProtectedAccess{
-	public static void main(String[] args) {
-		procaccess obj = new procaccess ();   
-	       obj.display();  
-	}
-}
